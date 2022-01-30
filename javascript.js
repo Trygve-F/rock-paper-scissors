077c8ec01c88d3cf7a5a6b06594d0e1d33f68018
Trygve-F <trygvefinley@gmail.com>
2022-01-29 16:12:24 -0800

    added game() function which replays game for until player or computer wins 5 rounds.
    changed prompts to alerts other than player input prompt
    
    added variables to keep track of playerWins and computerWins

--- javascript.js
+++ javascript.js
@@ -1,28 +1,7 @@
-//ROCK PAPER SCISSORS
 
-//begin game
-//generate prompt with 3 choices, rock paper or scissors
-let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
-
-//rock assign (1), paper assign (2) or scissors assign (3)
-let playerThrow 
-if (playerSelection === 'rock') {
-    playerThrow = 1;
-} else if (playerSelection === 'paper') {
-    playerThrow = 2;
-} else { 
-    playerThrow = 3;
-}
-
-
-
-
-//for computer;
-//generate random whole number between 0-3;
+function computerPlay() {
+let computerSelection;
 let computerThrow = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-
-//assign number to rock (1), paper (2), or scissors (3);
-let computerSelection
 if (computerThrow === 1) {
     computerSelection = 'rock';
 } else if (computerThrow ===2) {
@@ -30,37 +9,43 @@ if (computerThrow === 1) {
 } else {
     computerSelection = 'scissors'
 }
-/*
-console.log(playerThrow)
-console.log(playerSelection)
-console.log(computerThrow)
-console.log(computerSelection)
-*/
+return computerSelection
+}
 
-//for game;
-//compare computer number against players;
-function playRound (playerThrow, computerThrow) {
-    if (computerThrow === 1 && playerThrow === 3) {
-        prompt('You lose! Rock beats Scissors.');
-    } else if (computerThrow === 2 && playerThrow === 1) {
-        prompt('You lose! Paper beats Rock.')
-    } else if (computerThrow === 3 && playerThrow === 2) {
-        prompt('You Lose! Scissors beats Rock.')
-    } else {
-        prompt('You Win! ' + playerSelection + ' beats ' + computerSelection + '.')
+let playerWins = 0;
+let computerWins = 0;
+
+
+function game () {
+do { playRound();
+    } while (playerWins < 5 && computerWins <5) ;
+
+if (playerWins === 5) {
+    alert('You win! ' + 'Your score: ' + playerWins + ' Computer score: ' + computerWins)
+}
+else if (computerWins === 5) {
+    alert('You lose! ' + 'Your score: ' + playerWins + ' Computer score: ' + computerWins)
     }
 }
-playRound()
-//if computer number === player number: TIE play again;
-//else if computer number = 1 && player number = 3 computer wins
-//else if computer number = 2 && player number = 1 computer wins
-//else if computer number = 3 && player number = 2 computer wins
-//else player wins
-//go to end game
 
 
-//end game
-//pull computer choice and players choice
-//"you lose ____ beats ____"
-//"you win -player choice- beats -computer choice-"
-//reset game
+function playRound () {
+    let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+    let computerSelection = computerPlay();
+    if (computerSelection === playerSelection) {
+        alert('Its a Tie! Try again.');
+    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        alert('You lose! Rock beats Scissors.');
+        computerWins = computerWins + 1;
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        alert('You lose! Paper beats Rock.');
+        computerWins = computerWins + 1;
+    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        alert('You Lose! Scissors beats Paper.');
+        computerWins = computerWins + 1;
+    } else {
+        alert('You Win! ' + playerSelection + ' beats ' + computerSelection + '.');
+        playerWins = playerWins + 1;
+   }
+}
+game()
